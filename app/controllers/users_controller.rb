16e037a5012b9354b99db5076843f628d57ffed1
Robert Shields <34387120+rshield1@users.noreply.github.com>
2020-03-31 10:38:44 -0400

    setting up routes for users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,4 +4,27 @@ get '/signup' do
     erb :'/users/signup'
 end
 
+#taken from signup.erb form for params
+post "/users" do
+    user = User.create(params)
+    #check the validity of user
+    if user.valid?
+        session[:user_id] = user.id
+#user id is unique to each user
+        redirect to "/users/#{@user.id}"
+    else 
+        redirect to '/signup'
+end
+
+#create a dynamic route for id
+
+get '/users/:id' do
+    if User.find_by(id: params[:id])
+    @user = User.find_by(id: params[:id]) 
+    else
+       redirect to '/welcome' 
+    end
+    erb: '/users/show'
+end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,13 @@
 class User < ActiveRecord::Base
 has_many :recipes
 
+#gives you access to valid? method in your user class
+#it checks the validity of your user
+validates :username, presence: true uniqueness: true
+validates :email, presence: true uniqueness: true
+
+
+
 #secures the password with a params hash (bcrypt gem)
 has_secure_password
 
