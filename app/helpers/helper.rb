4eaf01881f8bef6b97fde3baecccbb85ee508a34
Robert Shields <34387120+rshield1@users.noreply.github.com>
2020-03-31 12:39:23 -0400

    added helper methods to check for validity

---
+++ app/helpers/helper.rb
@@ -0,0 +1,18 @@
+class Helpers
+
+
+def self.current_user(session)
+    #what user is supposed to be logged in
+    User.find_by(id: session[:user_id])
+
+end
+
+def self.is_logged_in?(session)
+    #is there a user id in our session?
+    session[:user_id] ? true : false
+end
+
+
+end
+
+end
