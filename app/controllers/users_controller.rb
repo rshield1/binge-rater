f53d3e6d6afcbdd381fcd5564186e299290438f3
Robert Shields <robert.shieldsiv@gmail.com>
2020-04-04 17:51:32 -0400

    fixed index styles and added delete account button

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -65,6 +65,14 @@ get '/users/:id' do
         erb  :'/users/show'
 end
 
+get '/users/:id' do
+    if Helpers.is_logged_in?(session) && User.find_by(id: params[:id])
+        user = User.find_by(username: params[:username])
+        user.delete
+        redirect to '/welcome' 
+    end
+end
+
 
 
 
